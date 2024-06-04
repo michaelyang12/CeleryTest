00da16d4bf93f63db69d44f2d097c1afe11870f2
michaelyang12 <michaelyang@wustl.edu>
2024-06-04 15:18:54 -0400

    remove custom logs

--- celery_config.py
+++ celery_config.py
@@ -9,24 +9,14 @@ CELERY_RESULT_BACKEND = f'redis://:{redis_password}@localhost:6379/0'
 
 celery = Celery(__name__, broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
-# Configure logging
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
-
 @celery.task(bind=True)
 def long_running_task(self):
     i = 0
     while True:
         if self.request.called_directly:
             break
-        logger.info(f"Task running... {i+1} seconds elapsed")
+        logging.info(f"Task running... {i+1} seconds elapsed")
         i += 1
         time.sleep(1)  # Simulate a long-running task
-    logger.info("Task completed")
+    logging.info("Task completed")
     return 'Task completed'
